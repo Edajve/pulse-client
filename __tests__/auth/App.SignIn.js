27f3eaa7a3d1db063adedde47c800cd1c216f296
Dajve Echols <dajvelechols@gmail.com>
2024-06-23 14:40:00 -0500

    add skipped test

--- __tests__/auth/App.SignIn.js
+++ __tests__/auth/App.SignIn.js
@@ -1,11 +1,22 @@
-import { fireEvent, render } from '@testing-library/react-native';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import React from 'react';
 import SignIn from '../../app/(auth)/sign-in.jsx';
 import { useGlobalContext } from '../../context/GlobalProvider';
+import { authenticate } from '../../app/lib/pulse-services.js';
+import { useRouter } from 'expo-router';
+import { getByTestId } from '@testing-library/react';
 
-// Mock useGlobalContext
+jest.mock('expo-router', () => ({
+    ...jest.requireActual('expo-router'), // Use actual implementation for other functions
+    useRouter: jest.fn(), // Mock useRouter function
+}));
+
 jest.mock('../../context/GlobalProvider.js', () => ({
     useGlobalContext: jest.fn(),
+}));
+
+jest.mock('../../app/lib/pulse-services.js', () => ({
+    authenticate: jest.fn(),
 }));
 
 describe('SignIn component test', () => {
@@ -58,5 +69,43 @@ describe('SignIn component test', () => {
         fireEvent.press(getByText('Log In'));
         const modal = await findByTestId('blur-modal-container');
         expect(modal).toBeTruthy();
+    });
+
+    test('should show pop up when user only puts in password field and not email as well', async () => {
+        const { getByPlaceholderText, getByText, findByTestId } = render(<SignIn />);
+        fireEvent.changeText(getByPlaceholderText('password'), 'testPassword');
+        fireEvent.press(getByText('Log In'));
+        const modal = await findByTestId('blur-modal-container');
+        expect(modal).toBeTruthy();
+    });
+
+    // still needs to be worked on, the react-router replace is not verifying correctly
+    test.skip('should navigate to home page after correct home page', async () => {
+        useRouter.mockReturnValue({
+            replace: jest.fn(),
+        });
+
+        authenticate.mockResolvedValue({
+            data: {
+                token: 'mockedToken',
+                id: 'mockedId',
+            },
+        });
+
+        const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+        fireEvent.changeText(getByPlaceholderText('email'), 'test@example.com');
+        fireEvent.changeText(getByPlaceholderText('password'), 'testPassword');
+        
+        const continueButton = getByTestId('loginBtn');
+        expect(continueButton).toBeTruthy()
+        fireEvent.press(continueButton);
+
+        await waitFor(() => {
+            expect(useGlobalContext().setToken).toHaveBeenCalledWith('mockedToken');
+            expect(useGlobalContext().setId).toHaveBeenCalledWith('mockedId');
+            expect(useGlobalContext().setIsLoggedIn).toHaveBeenCalledWith(true);
+            expect(useRouter().replace).toHaveBeenCalledWith('/home');
+        });
     });
 });
