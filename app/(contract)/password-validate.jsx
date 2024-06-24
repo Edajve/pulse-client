160882b234be4432d44fa0ca2d036f1bfc46b66d
Dajve Echols <dajvelechols@gmail.com>
2024-06-23 21:08:05 -0500

    formatting refactor on Authorize into Consent page

--- app/(contract)/password-validate.jsx
+++ app/(contract)/password-validate.jsx
@@ -1,6 +1,6 @@
 import { router } from "expo-router";
 import React, { useState } from 'react';
-import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Image, Text, TouchableOpacity, View, ScrollView } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from "react-native-safe-area-context";
 import icons from '../../constants/icons';
@@ -66,46 +66,48 @@ const PasswordValidate = () => {
   };
 
   return (
-    <GestureHandlerRootView className='w-full h-full'>
-      <SafeAreaView className="bg-primary h-full w-full">
-        <View className='w-full flex-col'>
-          <TouchableOpacity
-            className="flex-1 pt-5 pl-5"
-            onPress={() => router.replace('/create')}
-          >
-            <Image
-              className="w-[30px] h-[30px]"
-              source={icons.leftArrow}
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-        </View>
+    <ScrollView className="bg-primary h-full w-full">
+      <GestureHandlerRootView >
+        <SafeAreaView >
+          <View className='w-full flex-col'>
+            <TouchableOpacity
+              className="flex-1 pt-5 pl-5"
+              onPress={() => router.replace('/create')}
+            >
+              <Image
+                className="w-[30px] h-[30px]"
+                source={icons.leftArrow}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
 
-        <View className="w-full justify-center min-h-[80vh] px-4 my-6">
-          <Text className="text-base text-gray-200 font-medium mb-[50px] text-3xl font-pmedium">
-            Authorize into Consent
-          </Text>
-          <FormField
-            title='Consent Number - Make sure you and the other participant put in the same consent number to authorize into contract'
-            value={form.consentNumber}
-            handleChangeText={(e) => setForm({ ...form, consentNumber: e })}
-            otherStyles='mt-7'
-          />
-          <FormField
-            title='Your Password - To authorize you are you and you are cognizant'
-            value={form.password}
-            handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles='mt-7'
-          />
-          <CustomButton
-            title='Enroll in Consent Contract'
-            handlePress={handleEnrollInConsent}
-            containerStyle='mt-7'
-            disabled={isSubmitting} // Disable button while submitting
-          />
-        </View>
-      </SafeAreaView>
-    </GestureHandlerRootView>
+          <View className="w-full justify-center min-h-[60vh] px-4 my-6">
+            <Text className="text-base text-gray-200 font-medium mb-[50px] text-3xl font-pmedium">
+              Authorize into Consent
+            </Text>
+            <FormField
+              title='Consent Number - Make sure you and the other participant put in the same consent number to authorize into contract'
+              value={form.consentNumber}
+              handleChangeText={(e) => setForm({ ...form, consentNumber: e })}
+              otherStyles='mt-7'
+            />
+            <FormField
+              title='Your Password - To authorize you are you and you are cognizant'
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+              otherStyles='mt-7'
+            />
+            <CustomButton
+              title='Enroll in Consent Contract'
+              handlePress={handleEnrollInConsent}
+              containerStyle='mt-7'
+              disabled={isSubmitting} // Disable button while submitting
+            />
+          </View>
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </ScrollView>
   );
 };
 
