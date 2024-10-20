2f10b653aa7a2eb7d47bf66cc37b279622d9adda
Dajve Echols <dajvelechols@gmail.com>
2024-10-20 11:40:54 -0500

    clean up

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 
 const useApi = (apiFunction) => {
     const [loading, setLoading] = useState(false);
